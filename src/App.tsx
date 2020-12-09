41e7015858903478f3db4883f7fae922fec8bed0
Rob Lewis <rlewis@frontlineed.com>
2020-12-09 09:32:58 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup';
+
+type User = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthDate: string;
+  password: string;
+}
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  birthDate: Yup.string().required("Required!"),
+  password: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+});
 
 function App() {
+  const handleSubmit = (values: User) => {
+    alert(JSON.stringify(values))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container mt-2">
+
+      <h3>Basic Form using Bootstrap, Formik, Yup, & React</h3>
+      <div className="col-md-6">
+        <Formik
+          initialValues={{ firstName: '', lastName: '', email: '', birthDate: '', password: '' }}
+          onSubmit={(values) => handleSubmit(values)}
+          validationSchema={SignupSchema}
         >
-          Learn React
-        </a>
-      </header>
+          {({ errors, touched }) => (
+            <Form>
+              <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                <Field name="firstName" type="text" className="form-control" />
+                {errors.firstName && touched.firstName ? (
+                  <div className="text-danger">{errors.firstName}</div>
+                ) : null}
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="lastName">Last Name</label>
+                <Field name="lastName" type="text" className="form-control" />
+                {errors.lastName && touched.lastName ? (
+                  <div className="text-danger">{errors.lastName}</div>
+                ) : null}
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <Field name="email" type="email" className="form-control" />
+                {errors.email && touched.email ? (
+                  <div className="text-danger">{errors.email}</div>
+                ) : null}
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="birthDate">Birth Date</label>
+                <Field name="birthDate" type="date" className="form-control" />
+                {errors.birthDate && touched.birthDate ? (
+                  <div className="text-danger">{errors.birthDate}</div>
+                ) : null}
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <Field name="password" type="password" className="form-control" />
+                {errors.password && touched.password ? (
+                  <div className="text-danger">{errors.password}</div>
+                ) : null}
+              </div>
+              <button className="btn btn-primary" type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Fabric } from "@fluentui/react";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Fabric>
+      <App />
+    </Fabric>
   </React.StrictMode>,
   document.getElementById('root')
 );
